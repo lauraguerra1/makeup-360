2a91a83e4b4b01c23c60724dd9bdd3bb585841f8
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-07-24 01:23:37 -0600

    Add some styling.

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -21,10 +21,20 @@ const ProductDetail = ({allProducts}: ProductDetailProps) => {
   return (
     <div className='product-detail-card'>
       {chosenProductDetails ? (
-        <div>
-          <img src={chosenProductDetails.api_featured_image} className='product-image' id={chosenProductDetails.id.toString()} alt={chosenProductDetails.name}/>
-          <h1>{chosenProductDetails.name}</h1>
-          <p>Price: ${chosenProductDetails.price}</p>
+        <div className='product-detail-container'>
+
+            <div className='image-container'>
+              <Link to={chosenProductDetails.product_link} >
+                <img src={chosenProductDetails.api_featured_image} className='product-image' id={chosenProductDetails.id.toString()} alt={chosenProductDetails.name}/>
+              </Link>
+            </div>
+            <aside className='details-aside'>
+              <h1>{chosenProductDetails.name}</h1>
+              <p className='product-price'>Price: ${chosenProductDetails.price}</p><br />
+              <p className='product-description'>Description: {chosenProductDetails.description}</p><br />
+              <p className='product-rating'>Rating: {chosenProductDetails.rating}</p><br />
+            </aside>
+
         </div>
       ) : (
         <p>Product not found</p>

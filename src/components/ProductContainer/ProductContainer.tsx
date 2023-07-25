07032ddb82ef6cbf8fd02d020e06fa1c11fdf725
Fernando Robles <fernandorobles97@yahoo.com>
2023-07-25 00:10:21 -0500

    Create a variable that maps products with a rating equal to 5

--- src/components/ProductContainer/ProductContainer.tsx
+++ src/components/ProductContainer/ProductContainer.tsx
@@ -2,11 +2,12 @@ import { useEffect } from "react"
 import { Product } from "../../apiTypes"
 import ProductCard from "../ProductCard/ProductCard"
 interface PCProps {
+  featuredProducts: Product[]
   filteredProducts: Product[]
   searching: boolean
 }
 
-const ProductContainer = ({filteredProducts, searching}: PCProps) => {
+const ProductContainer = ({featuredProducts, filteredProducts, searching}: PCProps) => {
   useEffect(() => {
 
   }, [filteredProducts])
@@ -14,6 +15,21 @@ const ProductContainer = ({filteredProducts, searching}: PCProps) => {
 //this will contain all the little products or the featured items
 // will get a prop coming from App with all single products 
 //maps over them to return a bunch of <ProductCard /> 's 
+
+const featuredProductCards = featuredProducts.map(product => {
+  if(product.rating === 5) {
+    return (
+      <ProductCard 
+        image={product.api_featured_image}
+        brand={product.brand}
+        name={product.name}
+        tags={product.tag_list}
+        id={product.id}
+        key={product.id}
+      />
+    )
+  }
+})
 
 const productCards = filteredProducts.map(product => {
 
@@ -31,10 +47,10 @@ const productCards = filteredProducts.map(product => {
 // if the array of 'filtered /all products' is empty display featured items 
 //if featured items then add an H2 for FEATURED 
   return (
-  <p className="product-wrapper">
-    This will either be fatured products or all products 
-    {productCards}
-  </p>
+    <section className="product-wrapper">
+      {featuredProductCards}
+      {productCards}
+    </section>
   )
 }
 

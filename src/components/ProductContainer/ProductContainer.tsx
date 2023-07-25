36cc9195caf74e892ae75a89456b221575be9551
Fernando Robles <fernandorobles97@yahoo.com>
2023-07-25 18:03:25 -0500

    Create functions to get/update random products; create function to map product cards; conditionally render product cards

--- src/components/ProductContainer/ProductContainer.tsx
+++ src/components/ProductContainer/ProductContainer.tsx
@@ -1,55 +1,61 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Product } from "../../apiTypes"
 import ProductCard from "../ProductCard/ProductCard"
 interface PCProps {
+  updateFeaturedProducts: (newProducts:Product[]) => void
   featuredProducts: Product[]
+  allProducts: Product[]
   filteredProducts: Product[]
   searching: boolean
 }
 
-const ProductContainer = ({featuredProducts, filteredProducts, searching}: PCProps) => {
+const ProductContainer = ({allProducts, updateFeaturedProducts, featuredProducts, filteredProducts, searching}: PCProps) => {
   useEffect(() => {
-
-  }, [filteredProducts])
+    if(allProducts.length) {
+      updateRandomProducts(allProducts)
+    }
+  }, [allProducts])
 
 //this will contain all the little products or the featured items
 // will get a prop coming from App with all single products 
 //maps over them to return a bunch of <ProductCard /> 's 
 
-const featuredProductCards = featuredProducts.map(product => {
-  if(product.rating === 5) {
-    return (
-      <ProductCard 
-        image={product.api_featured_image}
-        brand={product.brand}
-        name={product.name}
-        tags={product.tag_list}
-        id={product.id}
-        key={product.id}
-      />
-    )
+const updateRandomProducts = (products: Product[]) => {
+  let fiveStars = products.filter(product => product.rating === 5) 
+  updateFeaturedProducts(getRandomProducts(fiveStars))
+}
+
+const getRandomProducts = (products:Product[]) => {
+  const newProducts = []
+  for(let i = 0; i < 4; i++) {
+    newProducts.push(products[Math.floor(Math.random()*products.length)])
   }
-})
+  return newProducts
+}
 
-const productCards = filteredProducts.map(product => {
+const getProductCards = (products:Product[]):JSX.Element[]=> {
+    return products.map(product => {
+      return (
+        <ProductCard 
+          image={product.api_featured_image}
+          brand={product.brand}
+          name={product.name}
+          tags={product.tag_list}
+          id={product.id}
+          key={product.id}
+        />
+      )
+  })
+  } 
 
-  return (
-    <ProductCard 
-      image={product.api_featured_image}
-      brand={product.brand}
-      name={product.name}
-      tags={product.tag_list}
-      id={product.id}
-    />
-  )
-});
+const featuredProductCards = getProductCards(featuredProducts)
+const filteredProductCards = getProductCards(filteredProducts)
 
 // if the array of 'filtered /all products' is empty display featured items 
 //if featured items then add an H2 for FEATURED 
   return (
     <section className="product-wrapper">
-      {featuredProductCards}
-      {productCards}
+      {filteredProducts.length ? filteredProductCards : featuredProductCards}
     </section>
   )
 }

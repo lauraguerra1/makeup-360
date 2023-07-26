413f501744ccb10cb91881f9780eea3749031a00
Fernando Robles <fernandorobles97@yahoo.com>
2023-07-26 10:53:19 -0500

    Add featuredProducts state to ProductContainer; set its state in updateRandomProducts function; add indentions

--- src/components/ProductContainer/ProductContainer.tsx
+++ src/components/ProductContainer/ProductContainer.tsx
@@ -1,36 +1,35 @@
-import { useEffect} from "react"
+import { useEffect, useState} from "react"
 import { Product } from "../../apiTypes"
 import ProductCard from "../ProductCard/ProductCard"
 import './ProductContainer.css'
 interface PCProps {
-  updateFeaturedProducts: (newProducts:Product[]) => void
-  featuredProducts: Product[]
   allProducts: Product[]
   filteredProducts: Product[]
-  searching: boolean
 }
 
-const ProductContainer = ({allProducts, updateFeaturedProducts, featuredProducts, filteredProducts, searching}: PCProps) => {
+const ProductContainer = ({allProducts, filteredProducts}: PCProps) => {
+  const [featuredProducts, setFeaturedProducts] = useState<Product[]>([])
+
   useEffect(() => {
     if(allProducts.length) {
       updateRandomProducts(allProducts)
     }
   }, [])
 
-const updateRandomProducts = (products: Product[]) => {
-  let fiveStars = products.filter(product => product.rating === 5) 
-  updateFeaturedProducts(getRandomProducts(fiveStars))
-}
-
-const getRandomProducts = (products:Product[]) => {
-  const newProducts = []
-  for(let i = 0; i < 4; i++) {
-    newProducts.push(products[Math.floor(Math.random()*products.length)])
+  const updateRandomProducts = (products: Product[]) => {
+    let fiveStars = products.filter(product => product.rating === 5) 
+    setFeaturedProducts(getRandomProducts(fiveStars))
   }
-  return newProducts
-}
 
-const getProductCards = (products:Product[]):JSX.Element[]=> {
+  const getRandomProducts = (products:Product[]) => {
+    const newProducts = []
+    for(let i = 0; i < 4; i++) {
+      newProducts.push(products[Math.floor(Math.random()*products.length)])
+    }
+    return newProducts
+  }
+
+  const getProductCards = (products:Product[]):JSX.Element[]=> {
     return products.map(product => {
       return (
         <ProductCard 
@@ -45,8 +44,8 @@ const getProductCards = (products:Product[]):JSX.Element[]=> {
   })
   } 
 
-const featuredProductCards = getProductCards(featuredProducts)
-const filteredProductCards = getProductCards(filteredProducts)
+  const featuredProductCards = getProductCards(featuredProducts)
+  const filteredProductCards = getProductCards(filteredProducts)
 
   return (
     <section className="product-container">

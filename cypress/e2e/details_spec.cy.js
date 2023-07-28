82301083c4bac9c571209c79bef2e25e9d2823ab
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-07-28 00:31:22 -0600

    Tests passing for two products and three user stories.

--- cypress/e2e/details_spec.cy.js
+++ cypress/e2e/details_spec.cy.js
@@ -1,7 +1,29 @@
 describe('view product details spec', () => {
 
-  Cypress.Commands.add('filterBy', (category) => {
-    cy.get('.category').contains(category).click()
+  Cypress.Commands.add('showDetails', (position, productId, brand, name, description, price, length, color) => {
+    cy.get('.product-card')[position]().click()
+      .url().should('eq', `http://localhost:3000/product/${productId}`)
+    cy.get('.product-detail-container')
+    cy.contains('h3', `${brand}`)
+    cy.contains('h4', `${name}`)
+    cy.get('.product-description')
+      .contains(`${description}`)
+    cy.get('.product-price')
+      .contains(`Price: $${price}`)
+    cy.get('.color-container')
+      .children().should('have.length', length)
+      .first().should('have.css', 'background-color', `${color}`)
+    cy.get('.buttons-container')
+    cy.get('.add-product-to-favorites').click()
+    cy.get('.favorites').click()
+      .url().should('eq', 'http://localhost:3000/favorites')
+    cy.get('.product-container')
+      .get('.product-card')
+      .get(`#${productId}`)
+    cy.get('.product-brand').contains(`${brand}`)
+    cy.get('.product-name').contains(`${name}`)
+      .go(-1).url().should('eq', `http://localhost:3000/product/${productId}`)
+    cy.get('.website-link-button')
   })
 
   beforeEach(() => {
@@ -9,39 +31,23 @@ describe('view product details spec', () => {
       statusCode: 200,
       fixture: 'products.json'
     }).as('getProducts')
-    cy.fixture('images').as('imagesData');
     cy.visit('http://localhost:3000')
-
   })
 
   it('should show a chosen product\'s detail page', () => {
-    cy.wait('@getProducts').then(() => {
+    cy.wait('@getProducts').then((interception) => {
       cy.get('.category').contains('Lipstick').click()
-      cy.get('.product-card').first().click()
-        .url().should('eq', 'http://localhost:3000/product/1047')
-      cy.get('.product-detail-container')
-      cy.contains('h3', 'colourpop')
-      cy.contains('h4', 'Blotted Lip')
-      cy.get('.product-description')
-        .contains('Blotted Lip Sheer matte lipstick that creates the perfect popsicle pout! Formula is lightweight, matte and buildable for light to medium coverage.')
-      cy.get('.product-price')
-        .contains('Price: $5.00')
-      cy.get('.color-container')
-        .children().should('have.length', 7)
-        .first().should('have.css', 'background-color', 'rgb(183, 34, 39)')
-      cy.get('.buttons-container')
+      cy.showDetails('first', 1047, 'colourpop', 'Blotted Lip', 'Blotted Lip Sheer matte lipstick that creates the perfect popsicle pout! Formula is lightweight, matte and buildable for light to medium coverage.', 5.00, 7, 'rgb(183, 34, 39)')
+      cy.get('.logo-link').click()
+        .url().should('eq', 'http://localhost:3000/')
+      cy.get('.category').contains('Foundation').click()
+      cy.showDetails('last', 1042, 'deciem', 'Coverage Foundation', 'Coverage Foundations are full-coverage formulations available in a comprehensive shade range across 21 shades. These foundations contain higher pigment levels than our Serum Foundations but still offer a smooth finish that avoids the heavy makeup look that can make skin appear more aged. The texture is that of a lightweight, non-oily cream.', 6.00, 21, 'rgb(244, 229, 214)')
+      cy.get('.product-rating').contains('Rating: 5')
       cy.get('.add-product-to-favorites').click()
       cy.get('.favorites').click()
         .url().should('eq', 'http://localhost:3000/favorites')
-      cy.get('.product-container')
-      cy.get('.product-card')
-        .get('#1047')
-      cy.get('.product-brand').contains('colourpop')
-      cy.get('.product-name').contains('Blotted Lip')
-        .go(-1).url().should('eq', 'http://localhost:3000/product/1047')
-        .get('.website-link-button')
-        
+      cy.get('.product-wrapper')
+        .should('have.length', 1)
     })
   })
 })
-

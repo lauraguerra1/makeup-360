9afc6b64632cc43d8a9006b03035833037f31492
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-27 21:09:14 -0400

    Turn entire product card into link

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -12,19 +12,16 @@ interface ProductCardProps {
 
 const ProductCard = (props: ProductCardProps) => {
   return (
-    <div className='product-card'>
+    <Link className='product-card' to={`/product/${props.id}`} >
       <div className='image-container'>
-        <Link to={`/product/${props.id}`} >
           <img className='product-img' src={props.image} id={props.id.toString()} alt={props.name}/>
-        </Link>
       </div>
       <h2 className='product-brand'>{props.brand}</h2>
       <p className='product-name'>{props.name.replace('&trade;', '\u2122')}</p>
       {props.tags.length ? (
         <p className='product-tags'>tags: {props.tags.join(', ')}</p>
         ) : null }
-
-    </div>
+    </Link>
   )
 }
 

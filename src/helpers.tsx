d22c63658e1e0cb7b5dfb43dc4acfde1f4c0e476
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-28 18:09:43 -0400

    Clean data from api and create apiData type

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,3 +1,5 @@
+import { Product, apiData } from "./apiTypes"
+
 const makeSnakeCase = (words: string | null) => words ? words.replaceAll(' ', '_').toLowerCase() : null
 
 const checkPage = (location: string, searching: boolean) => {
@@ -12,4 +14,22 @@ const checkPage = (location: string, searching: boolean) => {
   }
 }
 
-export {makeSnakeCase, checkPage}
+const cleanAPIData = (product: apiData): Product => {
+ const cleanedProduct:Product = {
+  api_featured_image: product.api_featured_image,
+  brand: product.brand,
+  description: product.description,
+  id: product.id,
+  name: product.name,
+  price: product.price,
+  product_colors: product.product_colors,
+  product_link: product.product_link,
+  product_type: product.product_type,
+  rating: product.rating,
+  tag_list: product.tag_list,
+ }
+
+ return cleanedProduct
+}
+
+export {makeSnakeCase, checkPage, cleanAPIData}

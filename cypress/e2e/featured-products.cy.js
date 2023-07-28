0d284e8d353d6c1d89772e873102252bc4eb493d
Fernando Robles <fernandorobles97@yahoo.com>
2023-07-28 17:00:34 -0500

    Refactor to only have one it block

--- cypress/e2e/featured-products.cy.js
+++ cypress/e2e/featured-products.cy.js
@@ -1,7 +1,8 @@
 describe('Featured Products Section', () => {
 
   Cypress.Commands.add('checkRatingAndUniquity', (name) => {
-    cy.get(`.featured-card:contains(${name})`).find('img[alt="five star rating"]').should('have.length', 1)
+    cy.get(`.featured-card:contains(${name})`).should('have.length', 1)
+    .find('img[alt="five star rating"]')
   })
 
   beforeEach(() => {
@@ -13,18 +14,11 @@ describe('Featured Products Section', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('Should display 4 product cards with rating strictly equal to 5', () => {
+  it('Should display 4 unique product cards with ratings strictly equal to 5', () => {
     cy.get('.product-wrapper').find('.product-card').should('have.length', 4)
-    .checkRating('Serum Foundation')
-    .checkRating('Coverage Foundation')
-    .checkRating('Precision Brow Pencil')
-    .checkRating('Sculpt & Highlight Brow Contour')
-  })
-
-  it('Should only contain unique products', () => {
-    cy.checkUniquity('Serum Foundation')
-    .checkUniquity('Coverage Foundation')
-    .checkUniquity('Precision Brow Pencil')
-    .checkUniquity('Sculpt & Highlight Brow Contour')
+    .checkRatingAndUniquity('Serum Foundation')
+    .checkRatingAndUniquity('Coverage Foundation')
+    .checkRatingAndUniquity('Precision Brow Pencil')
+    .checkRatingAndUniquity('Sculpt & Highlight Brow Contour')
   })
 })

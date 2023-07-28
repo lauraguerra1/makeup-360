cc0274291e861eab6a59d5ca3e50e6cfb85affb9
Laura Guerra <121131581+lauraguerra1@users.noreply.github.com>
2023-07-28 16:39:07 -0400

    Fetch from secure source

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -8,7 +8,7 @@ const handleError = (response: Response): Promise<Product[]> => {
 }
 
 const getAllProducts = async(): Promise<Product[]> => {
-  let response = await fetch('http://makeup-api.herokuapp.com/api/v1/products.json')
+  let response = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json')
   let data = await handleError(response)
   return data
 }

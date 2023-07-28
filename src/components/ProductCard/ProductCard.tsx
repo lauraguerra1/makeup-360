f3fb467febe11567ff588d30d3c405d649ad79cf
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-27 21:11:01 -0400

    Change tags display

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -11,16 +11,22 @@ interface ProductCardProps {
 }
 
 const ProductCard = (props: ProductCardProps) => {
+  const mainTags = ['Vegan', 'Organic', 'cruelty free']
+  const tags = props.tags.filter(tag => mainTags.some(main => main === tag))
+  const tagEls = tags.map(tag => {
+    return <p className='product-tags' key={tag}>{tag}</p>
+  })
+
   return (
     <Link className='product-card' to={`/product/${props.id}`} >
+      <div className="tags-container">
+        {tagEls}
+      </div>
       <div className='image-container'>
-          <img className='product-img' src={props.image} id={props.id.toString()} alt={props.name}/>
+        <img className='product-img' src={props.image} id={props.id.toString()} alt={props.name}/>
       </div>
       <h2 className='product-brand'>{props.brand}</h2>
       <p className='product-name'>{props.name.replace('&trade;', '\u2122')}</p>
-      {props.tags.length ? (
-        <p className='product-tags'>tags: {props.tags.join(', ')}</p>
-        ) : null }
     </Link>
   )
 }
